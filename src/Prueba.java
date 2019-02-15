174b323b8c201f59f76075d95cde9dfe8bb7b4ad
Cristofer Casas <cristofer32513@gmail.com>
2019-02-14 19:18:54 -0600

    Se creo la clase VentanaPrincipal y se configuro la ventana principal.

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,7 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
+class VentanaPrincipal extends JFrame {
+	
+	
+	public VentanaPrincipal() {
+		//Configuracion de la ventana
+		getContentPane().setLayout(new BorderLayout());
+		setTitle("Lab Master - Test Booking");
+		setSize(1350, 750);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+}
+
 
 public class Prueba {
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			//UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new VentanaPrincipal();
+			}
+		});
 	}
 }
